61e88fa2cf23a45376ce6d56757987ffe6a48ae4
bfilin <network.project9@gmail.com>
2021-09-13 16:44:28 -0400

    win"

--- win_setup_remote.bat
+++ win_setup_remote.bat
@@ -5,52 +5,64 @@ IF "%~1" == "" GOTO ExitLabel
 
 IF "%~2" == "" GOTO ExitLabel
 
-set EXTERNAL_IP=%1
+set EXTERNAL_IP=%1%
+set "ok2go=NOK"
+
 
 IF "%2%"=="basic" (GOTO Basic) ELSE ^
-IF "%2%"=="serveraUP" (GOTO CheckServerA) ELSE ^
-IF "%2%"=="serverbUP" (GOTO CheckServerB) ELSE ^
+IF "%2%"=="serveraUP" (GOTO StartServerA) ELSE ^
+IF "%2%"=="serverbUP" (GOTO StartServerB) ELSE ^
 IF "%2%"=="stopall" (GOTO StopAll) ELSE ^
+IF "%2%"=="vncport" (GOTO getVNCPort) ELSE ^
 IF "%2%"=="status" (GOTO ShowStatus) ELSE ^
 GOTO ExitLabel
 
+:checkGoOn
+ssh -o StrictHostKeyChecking=no -i id_rha_foundation0 kiosk@%EXTERNAL_IP% "source /usr/local/lib/rhttool.shlib && is_running bastion" || GOTO ExitBastion
+ssh -o StrictHostKeyChecking=no -i id_rha_foundation0 kiosk@%EXTERNAL_IP% "source /usr/local/lib/rhttool.shlib && is_running workstation" || GOTO ExitWorkstation
+set "ok2go=OK"
+GOTO %ret%
+
+:getVNCPort
+if "%ok2go%" == "NOK" (set "ret=getVNCPort" && GOTO :checkGoOn) ELSE (echo 'OK Basion and Workstation are running')
+
+ssh -o StrictHostKeyChecking=no -i id_rha_foundation0 kiosk@%EXTERNAL_IP% "virsh -c 'qemu:///system' vncdisplay workstation" > tmpval.txt
+set /p port=<tmpval.txt
+set port=590%port::=%
+echo vnc port is: %port%
+set "ok2go=NOK"
+GOTO Done
 
 
 :Basic
 echo attempting to start bastion nested VM on %EXTERNAL_IP%
-ssh -o StrictHostKeyChecking=no -i id_rha_foundation0 kiosk@%EXTERNAL_IP% "rht-vmctl start bastion" || GOTO ExitBastion
 
+ssh -o StrictHostKeyChecking=no -i id_rha_foundation0 kiosk@%EXTERNAL_IP% "source /usr/local/lib/rhttool.shlib && is_running bastion && echo bastion is alrady running || rht-vmctl start bastion" || GOTO ExitBastion
+echo --
 echo attempting to start workstation nested VM on %EXTERNAL_IP%
-ssh -o StrictHostKeyChecking=no -i id_rha_foundation0 kiosk@%EXTERNAL_IP% "rht-vmctl start workstation" || GOTO ExitWorkstation
+ssh -o StrictHostKeyChecking=no -i id_rha_foundation0 kiosk@%EXTERNAL_IP% "source /usr/local/lib/rhttool.shlib && is_running workstation && echo workstation is alrady running || rht-vmctl start workstation" || GOTO ExitWorkstation
+echo --
 
-echo attempting to ssh into %EXTERNAL_IP% and port forward port 5902
-ssh -C -i ./id_rha_foundation0 -L 5902:localhost:5902 kiosk@%EXTERNAL_IP% || GOTO Exit5902
+call :getVNCPort
+echo --
+
+echo attempting to ssh into %EXTERNAL_IP% and port forward port %port%
+ssh -C -i ./id_rha_foundation0 -L %port%:localhost:%port% kiosk@%EXTERNAL_IP% || GOTO ExitVNC
 EXIT /B 0
 GOTO Done
 
 
 :StartServerA
+if "%ok2go%" == "NOK" (set "ret=StartServerA" && GOTO :checkGoOn) ELSE (echo 'OK Basion and Workstation are running')
 ssh -o StrictHostKeyChecking=no -i id_rha_foundation0 kiosk@%EXTERNAL_IP%  "rht-vmctl start servera"
+set "ok2go=NOK"
 GOTO Done
-
-:CheckServerA
-echo attempting to start servera nested VM on %EXTERNAL_IP%
-ssh -o StrictHostKeyChecking=no -i id_rha_foundation0 kiosk@%EXTERNAL_IP% "source /usr/local/lib/rhttool.shlib && is_running bastion && is_running workstation && echo OK || echo NOK" > tmpval.txt
-set /p r=<tmpval.txt
-if "%r%" == "OK" (del tmpval.txt && GOTO StartServerA) ELSE (del tmpval.txt && echo Not possible, you need to run basic command first)
-GOTO Done
-
 
 
 :StartServerB
+if "%ok2go%" == "NOK" (set "ret=StartServerB" && GOTO :checkGoOn) ELSE (echo 'OK Basion and Workstation are running')
 ssh -o StrictHostKeyChecking=no -i id_rha_foundation0 kiosk@%EXTERNAL_IP%  "rht-vmctl start serverb"
-GOTO Done
-
-:CheckServerB
-echo attempting to start serverb nested VM on %EXTERNAL_IP%
-ssh -o StrictHostKeyChecking=no -i id_rha_foundation0 kiosk@%EXTERNAL_IP% "source /usr/local/lib/rhttool.shlib && is_running bastion && is_running workstation && echo OK || echo NOK" > tmpval.txt
-set /p r=<tmpval.txt
-if "%r%" == "OK" (del tmpval.txt && GOTO StartServerB) ELSE (del tmpval.txt && echo Not possible, you need to run basic command first)
+set "ok2go=NOK"
 GOTO Done
 
 
@@ -84,26 +96,27 @@ GOTO Done
 
 
 :ExitWorkstation
-echo Could not start workstation, are you sure you have provided the correct IP address and that VM is ON
-GOTO Done
+echo workstation is not running, are you sure you have provided the correct IP address and that VM is ON
+EXIT /B 1
 
 :ExitBastion
-echo Could not start bastion, are you sure you have provided the correct IP address and that VM is ON
-GOTO Done
+echo bastion is not running, are you sure you have provided the correct IP address and that VM is ON
+EXIT /B 1
+
 
 :ExitServerA
 echo Could not start servera, are you sure you have provided the correct IP address and that VM is ON
-GOTO Done
+EXIT /B 1
 
 :ExitServerB
 echo Could not start serverb, are you sure you have provided the correct IP address and that VM is ON
-GOTO Done
+EXIT /B 1
 
 :ExitStopAll
 echo Could not stop the nested VMs
 GOTO Done
 
-:Exit5902
+:ExitVNC
 echo Could not port forward
 GOTO Done
 
